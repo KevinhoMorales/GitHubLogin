0cc0276d4f7e312da72714c7591c54b44dc4c1ca
Kevinho Morales <kfmorales94@gmail.com>
2020-06-14 14:36:50 -0500

    First Upload

---
+++ GithubSignInExample/GithubConstants.swift
@@ -0,0 +1,19 @@
+//
+//  GithubConstants.swift
+//  GithubSignInExample
+//
+//  Created by John Codeos on 12/14/19.
+//  Copyright © 2019 John Codeos. All rights reserved.
+//
+
+import Foundation
+
+struct GithubConstants {
+
+    static let CLIENT_ID = "Iv1.f0f335aa0502420d"
+    static let CLIENT_SECRET = "ebb8072d1623a6628edccc8d857949c8c28380a3"
+    static let REDIRECT_URI = "https://doistyapp.firebaseapp.com/__/auth/handler"
+    static let SCOPE = "read:user,user:email"
+    static let TOKENURL = "https://github.com/login/oauth/access_token"
+    
+}
